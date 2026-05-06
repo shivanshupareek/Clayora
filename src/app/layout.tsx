8293d0e37e7f9addff734dd0d4c99602fa28c856
shivanshupareek <shivanshupareek.ps@gmail.com>
2026-05-06 16:38:38 +0530

    scroll smooth behaviour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -194,6 +194,7 @@ export default function RootLayout({
     <html
       lang="en-AU"
       suppressHydrationWarning
+      data-scroll-behavior="smooth"
       className={`${inter.variable} ${poxe.variable}`}
     >
       <body>
